1cac9423c8f54c9ccb5c400464ba911c4e2d1017
joaquinmartin25-ai <joaquinmartin.25@campuscamara.es>
2026-02-26 20:43:16 +0100

    Modificaciones en scripts

--- Assets/_MyAssets/Scripts/Controllers/FlechaController.cs
+++ Assets/_MyAssets/Scripts/Controllers/FlechaController.cs
@@ -1,9 +1,12 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Flecha : MonoBehaviour
 {
     private Rigidbody rb;
-
+    public static int puntuacion = 0;
+    public TextMeshPro text;
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
@@ -19,6 +22,13 @@ public class Flecha : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        Destroy(gameObject);
+        
+        Debug.Log("Tu puntuacion es de: " +puntuacion);
+        if (collision.gameObject.CompareTag("Diana"))
+        {
+            puntuacion++;
+            text.SetText("Puntuacion: " + puntuacion);
+            Destroy(gameObject);
+        }
     }
 }
